349483c077109e75df84bcffda0888999e40d68d
J_Top <jetsada.sues@northbkk.ac.th>
2022-11-15 13:44:18 +0700

    add select

--- src/page/class/class/class.js
+++ src/page/class/class/class.js
@@ -6,6 +6,12 @@ import { mockTableClassList } from "../../../tableList";
 
 import "./class.css";
 
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
 const mockClassData = { detailTable:mockTableClassList };
 // const defaultClassData = { datailTable: [] };
 
@@ -31,6 +37,18 @@ export default function Classl() {
           </div>
           <div className="classl_header">
           </div>
+        </div>
+        <div className="select_year">
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <InputLabel id="demo-simple-select-standard-label">Year</InputLabel>
+            <Select>
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={1}>1/2565</MenuItem>
+              <MenuItem value={2}>2/2565</MenuItem>
+            </Select>
+          </FormControl>
         </div>
           <div className="class_table">
           <TableC dataList={mockClassData.detailTable} />
