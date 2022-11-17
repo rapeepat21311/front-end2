99d7cea4b3c8cb75f7b222648f13e32ac4aeec81
azz159753 <azz159753@hotmail.com>
2022-11-17 08:58:29 +0700

    s

--- src/page/news/news.js
+++ src/page/news/news.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../component/navbar/Header/Header";
+import { recommentNews } from "./recommentEvent";
 import "./news.css";
 
-function news() {
+//imgae material
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
+import RoomIcon from "@mui/icons-material/Room";
+
+function News() {
+  const [newsRecomment, setNewsRecomment] = useState(recommentNews);
   return (
     <>
       <Header />
@@ -14,10 +21,38 @@ function news() {
           <div className="news_title">
             <h1>กิจกรรมแนะนำ</h1>
           </div>
+          <div className="box_news_recomment">
+            <div className="box_news_img">
+              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAM1BMVEUAAAD////j4+MeHh709PQ1NTUoKCjLy8vZ2dkLCwtFRUUkJCT4+Pjr6+srKytKSko9PT3r1lA8AAABtElEQVR4nO3dS4rCABBF0Rh/+dm6/9X2sGdFHpSg6XMX8PCEOCvIMASdx9P+LsnycAmWx3M0nURIWERI2BQhYREhYVOEhEWEhE0REhYREjZFSFhESNgUIWERIWFThIRFhIRNERIWERI2RfjPhFs0vb1PeA76uY/7m+ZgeZ6C5ftP8qOHYHm8P6/7W5IfPS3B8jN50OMQvB2n8Rq8HGvy4m1rsHxN/iyn9wlvkfBGSEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISEhISPjVwo+5ibrsb5uW9ba39fUIph+vYHmZtmA6eHTDME/b/h7R9CNYnuZoOskFLWERIWFThIRFhIRNERIWERI2RUhYREjYFCFhESFhU4SERYSETRESFhESNkVIWERI2BQhYdGHCL/zru34t4nB2/Gl96VvEx7/RpiQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQ8K+PuYk6/rdkj/894KTv/KZz0vEvaAkJiwgJmyIkLCIkbIqQsIiQsClCwiJCwqYICYsICZsiJCwiJGyKkLCIkLApQsIiQsKmCD9S+AvTV48FOfTaXQAAAABJRU5ErkJggg==" />
+              <div className="news_recomment_date">
+                <h3>28</h3>
+                <p>ส.ค</p>
+              </div>
+            </div>
+            <div className="news_recomment_description">
+              <div className="recomment_news_title">{newsRecomment.title}</div>
+              <div className="news_description">
+                {newsRecomment.description}
+              </div>
+              <div className="news_location">
+                <RoomIcon />
+
+                <p>{newsRecomment.location}</p>
+              </div>
+              <div className="news_time_scope">
+                <AccessTimeFilledIcon />
+                <p>{newsRecomment.timeScope}</p>
+              </div>
+              <div className="news_event_scope">
+                <AccessTimeFilledIcon />
+                <p>{newsRecomment.eventScope}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 }
 
-export default news;
+export default News;
---
+++ src/page/news/recommentEvent.js
@@ -0,0 +1,9 @@
+export const recommentNews = {
+  title:
+    "กิจกรรม 88 SANDBOX จับมือกว่า 30 พันธมิตร ตอกย้ำบทบาท ECOSYSTEM ผลักดัน STARTUP ไทยสู่ระดับโลก",
+  description:
+    "88 SANDBOX พัฒนา startup ในงาน 88 Match & Member รวบรวมดาวเด่นด้าน Smart City 2022",
+  location: "Thamasat Grand Hall",
+  timeScope: "8:00 น. - 15.00 น.",
+  eventScope: "กิจกรรมภายใน",
+};
